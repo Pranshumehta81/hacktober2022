5d999fa202f3e4f367a2a49d645de0c0b1df5c62
Pranshumehta81 <72910904+Pranshumehta81@users.noreply.github.com>
2022-10-05 22:36:26 +0530

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+    // this just for computing it
     bool comp(vector<int> &a, vector<int> &b)
     {
         return (a[1] < b[1]);
@@ -11,6 +12,11 @@ public:
         int totalDur = 0;
         priority_queue<int> pq;
         int count = 0;
+        for (int m=0;m<courses.size();m++)
+        {
+            m++;
+        }
+        
         for (int i = 0; i < courses.size(); i++)
         {
             if (courses[i][0] + totalDur <= courses[i][1])
