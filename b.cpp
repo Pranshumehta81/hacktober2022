aaa2d4f3e517839a5311c1168ad391ac58605add
Pranshumehta <69533679+Pranshumehta@users.noreply.github.com>
2022-10-05 22:28:06 +0530

    Create b.cpp

---
+++ b.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int solver(int i, int j, int maxMove, int r, int c, vector<vector<vector<int>>> &dp)
+    {
+        int modu = 1e9 + 7;
+        if (i < 0 || j < 0 || i >= r || j >= c)
+        {
+            return 1;
+        }
+        if (maxMove == 0)
+        {
+            return 0;
+        }
+        if (dp[i][j][maxMove] != -1)
+        {
+            return dp[i][j][maxMove];
+        }
+
+        int res = 0;
+        res += solver(i + 1, j, maxMove - 1, r, c, dp) % modu;
+        res += solver(i, j + 1, maxMove - 1, r, c, dp) % modu;
+        res += solver(i - 1, j, maxMove - 1, r, c, dp) % modu;
+        res += solver(i, j - 1, maxMove - 1, r, c, dp) % modu;
+        return dp[i][j][maxMove] = res % modu;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn)
+    {
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove + 1, -1)));
+        return solver(startRow, startColumn, maxMove, m, n, dp);
+    }
+};
