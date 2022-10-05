3fd938d5c4c1560d59f3e53f96263c9d3405c384
Pranshumehta <69533679+Pranshumehta@users.noreply.github.com>
2022-10-05 22:28:45 +0530

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    bool comp(vector<int> &a, vector<int> &b)
+    {
+        return (a[1] < b[1]);
+    }
+    int scheduleCourse(vector<vector<int>> &courses)
+    {
+        sort(courses.begin(), courses.end(), comp);
+        int totalDur = 0;
+        priority_queue<int> pq;
+        int count = 0;
+        for (int i = 0; i < courses.size(); i++)
+        {
+            if (courses[i][0] + totalDur <= courses[i][1])
+            {
+                count++;
+                totalDur += courses[i][0];
+                pq.push(courses[i][0]);
+            }
+            else
+            {
+                if (pq.top() >= courses[i][0])
+                {
+                    int x = pq.top();
+                    totalDur -= x;
+                    pq.push(courses[i][0]);
+                }
+            }
+        }
+
+        return count;
+    }
+};
